e5ec519983764b5abcac86459d6d824da111a260
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-02-15 17:01:04 -0200

    shit wont work

--- lib/playlist.rb
+++ lib/playlist.rb
@@ -3,11 +3,10 @@ module RSpotify
   class Playlist < Base
     def remove_tracks(tracks)
       url = "#{@href}/tracks"
-      User.oauth_delete(@owner.id, url)
-      add_tracks(tracks)
+      puts "URL #{url} TRACKS #{tracks}"
+      User.oauth_delete(@owner.id, url, tracks)
       @total -= tracks.size
       @tracks_cache = nil
-      tracks
     end
   end
 end
