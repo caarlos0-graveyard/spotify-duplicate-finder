b46f4b8ee51f068573372938d15b20f2255bf216
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-02-15 16:20:10 -0200

    working some shit off

--- lib/playlist.rb
+++ lib/playlist.rb
@@ -2,13 +2,7 @@
 module RSpotify
   class Playlist < Base
     def remove_tracks(tracks)
-      data = {
-        tracks: tracks.map { |track| { uri: track.uri } }
-      }
-      puts data
-      puts data.to_json
-      url = "#{@href}/tracks?tracks=#{data.to_json}"
-      puts "URL #{url}"
+      url = "#{@href}/tracks"
       User.oauth_delete(@owner.id, url)
       add_tracks(tracks)
       @total -= tracks.size
--- lib/track_remover.rb
+++ lib/track_remover.rb
@@ -1,4 +1,5 @@
 require 'rspotify'
+require_relative "playlist_track_loader"
 
 class TrackRemover
   def initialize(playlist)
@@ -6,6 +7,20 @@ class TrackRemover
   end
 
   def remove(tracks)
-    @playlist.remove_tracks(RSpotify::Track.find(tracks))
+    tracks_to_remove = []
+    return tracks_to_remove unless tracks
+    PlaylistTrackLoader.new(@playlist).all.each_with_index do |track, index|
+      tracks.each do |other_track|
+        if other_track.eql? track.id
+          tracks_to_remove << {
+            uri: track.uri,
+            positions: [index]
+          }
+        end
+      end
+      tracks.delete(track.id)
+    end
+    puts tracks_to_remove
+    @playlist.remove_tracks(tracks_to_remove)
   end
 end
